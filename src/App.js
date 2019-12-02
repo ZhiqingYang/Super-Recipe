8444817965c653dcd7296b0082752fc0b53456a4
Jaren <tillej@uw.edu>
2019-12-01 22:56:49 -0800

    added search history list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchHistory from './SearchHistory';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchName: "",
+      savedSearches: []
+    };
+  }
+
+  render = () => {
+    return (
+      <div>
+        {this.state.savedSearches.length > 0 && (
+          <SearchHistory
+            searchHistory={this.state.searchHistory}
+            onSearchHistoryClicked={(search) => {
+              //this.makeRecipeQuery(search);
+            }}
+          />
+        )}
+      </div>
+    )
+  }
+};
 
 export default App;
