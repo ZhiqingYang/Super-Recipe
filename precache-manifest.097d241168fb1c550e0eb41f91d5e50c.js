1d8e5de54d376b4a4c0b21618b2fd041709168c5
Grady Thompson <gradyat@uw.edu>
2019-12-02 21:01:48 -0800

    Updates

---
+++ precache-manifest.097d241168fb1c550e0eb41f91d5e50c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e888008f8c579182f1301bbb5eff7105",
+    "url": "/team6-challenge9/index.html"
+  },
+  {
+    "revision": "53ade9e068e7db4be8ba",
+    "url": "/team6-challenge9/static/css/main.f1bc49f7.chunk.css"
+  },
+  {
+    "revision": "c2cd2fd4130af0636884",
+    "url": "/team6-challenge9/static/js/2.4bcc7e9b.chunk.js"
+  },
+  {
+    "revision": "53ade9e068e7db4be8ba",
+    "url": "/team6-challenge9/static/js/main.5aa1ad4d.chunk.js"
+  },
+  {
+    "revision": "17c6e7d484f7fda360de",
+    "url": "/team6-challenge9/static/js/runtime-main.12c7fed0.js"
+  },
+  {
+    "revision": "f42222914d685599fcb7e9364448ca61",
+    "url": "/team6-challenge9/static/media/balanced-diet.f4222291.jpg"
+  },
+  {
+    "revision": "94fdcd0e3961f932e959ccc58d0d0c11",
+    "url": "/team6-challenge9/static/media/chef.94fdcd0e.svg"
+  },
+  {
+    "revision": "6700e69d892440d0f513f243aed1709b",
+    "url": "/team6-challenge9/static/media/high-fiber.6700e69d.jpg"
+  },
+  {
+    "revision": "3293d7fa74b0d2c002b62fc2972cd199",
+    "url": "/team6-challenge9/static/media/high-protein.3293d7fa.jpg"
+  },
+  {
+    "revision": "4e6be69f85e9bbfb8558fb454ff7c24b",
+    "url": "/team6-challenge9/static/media/image1.4e6be69f.jpg"
+  },
+  {
+    "revision": "3ab3f8fb6afa2bee6ead317ad49ed570",
+    "url": "/team6-challenge9/static/media/image2.3ab3f8fb.jpg"
+  },
+  {
+    "revision": "a7bf2584912228673ad9fe24c2659d26",
+    "url": "/team6-challenge9/static/media/image3.a7bf2584.jpg"
+  },
+  {
+    "revision": "2e76f68dca79ca5041e80786826e55e1",
+    "url": "/team6-challenge9/static/media/image4.2e76f68d.jpg"
+  },
+  {
+    "revision": "a66391d7cf6b93882123acc46e0fcfe0",
+    "url": "/team6-challenge9/static/media/image5.a66391d7.jpg"
+  },
+  {
+    "revision": "fcc16fd1ced9d406e9b3241136ff4aaf",
+    "url": "/team6-challenge9/static/media/low-carb.fcc16fd1.jpg"
+  },
+  {
+    "revision": "5fdac98b41013ed6c2db6708e2ec9d77",
+    "url": "/team6-challenge9/static/media/low-fat.5fdac98b.png"
+  },
+  {
+    "revision": "ad5064044e31683f0916736363811d9a",
+    "url": "/team6-challenge9/static/media/low-sodium.ad506404.jpg"
+  }
+]);
