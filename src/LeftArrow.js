58dbc7e5a92d079d7630e161a298d4ad8a40de50
yang05-1760239 <yang05@uw.edu>
2019-12-04 19:26:55 -0800

    styled slider

--- src/LeftArrow.js
+++ src/LeftArrow.js
@@ -1,25 +1,14 @@
 import React from 'react';
-
-// const LeftArrow = (props) => {
-//     return (
-//         <div className="backArrow" onClick={props.goToPrevSlide}>
-//             <i className="fa fa-arrow-left fa-2x" aria-hidden="true" />
-//         </div>
-//     );
-// };
-
-
-
+import './App.css';
 
 class BackArrow extends React.Component {
     render = (props) => {
         return (
         <div 
-        onClick={this.props.previousImage} 
-        style={{fontSize: '2em', marginRight: '12px', color: 'black', position: "absolute", top: "50%"}}>
-        <button id="left-arrow">
-            <i className="arrow fa fa-chevron-left" aria-hidden="true"></i>
-        </button>
+            onClick={this.props.previousImage} >
+            <a href="/#">
+                <i  className="arrow fa fa-chevron-left" aria-hidden="true"  id="left-arrow"/>
+            </a>
            
         </div>
         );
--- src/RightArrow.js
+++ src/RightArrow.js
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import './App.css';
 
 
 class NextArrow extends React.Component {
     render = (props) => {
         return (
           <div 
-          onClick={this.props.nextImage}
-          style={{fontSize: '2em', marginRight: '12px', color: 'black', position: "absolute", top: "50%", right: "250px"}}>
-        <button id="right-arrow">
-            <i className="arrow fa fa-chevron-right" aria-hidden="true"></i>
-        </button>
+          onClick={this.props.nextImage}>
+            <a href="/#" >
+                <i className="arrow fa fa-chevron-right" aria-hidden="true"  id="right-arrow">
+                </i>
+            </a>
            
         </div>
         );
