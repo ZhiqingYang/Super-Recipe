d464890e04976dbea8ebd5c073f394f875dd7d6f
Jaren <tillej@uw.edu>
2019-12-05 01:32:16 -0800

    styled history category spacing

--- src/SearchHistory.js
+++ src/SearchHistory.js
@@ -1,32 +1,30 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import Recipe from './Recipe';
 
 // import bootstrap
 class SearchHistory extends React.Component {
     render = () => {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <h2 id="history-title">Recipe Search History</h2>
-                        <ul className="history-list">
-                            {this.props.searchHistory.map((search) => {
-                                return (
-                                    <li key={search.recipe.label}>
-                                        <p className="history-labels">{search.recipe.label}</p>
-                                        <a id="searchName" href="/#" onClick={(e) => {
-                                            e.preventDefault();
-                                            this.props.onClick(search);
-                                        }}>
-                                            <img className="history-images" src={search.recipe.image} alt="recipe search" />
-                                        </a>
-                                    </li>
-                                );
-                            })}
+            <div className="history-list">
+                <h2 id="history-title">Recipe Search History</h2>
+
+                {this.props.searchHistory.map((search) => {
+                    return (
+                        <ul className="list-group-flush">
+                            <li className="list-group-item" key={search.recipe.label}>
+
+                                <a id="searchName" href="/#" onClick={(e) => {
+                                    e.preventDefault();
+                                    this.props.onClick(search);
+                                }}>
+                                    <img className="history-images" src={search.recipe.image} alt="recipe search" />
+                                    <p className="history-labels">{search.recipe.label}</p>
+                                </a>
+                            </li>
                         </ul>
-                    </div>
-                </div>
+                    );
+                })}
+
             </div>
         );
     }
